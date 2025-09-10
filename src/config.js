a7067f494791315982ab885df2bf5449ffc452d4
robertcohenpmi <robert.cohen.pmi@gmail.com>
2025-09-10 17:41:50 +0100

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const JSON_PATH = 'WI_Export.json';
+export const SALESFORCE_DOMAIN = 'https://mypmi.lightning.force.com';
+export const THEME_KEY = 'pc_wi_theme';
+export const STATE_KEY = 'pc_wi_state_v2';
